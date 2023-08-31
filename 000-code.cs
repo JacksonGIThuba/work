6e1ce4e0d63d0227e6b80a74d073417e603b8310
JacksonGIThuba <141899073+JacksonGIThuba@users.noreply.github.com>
2023-08-31 15:52:02 +1000

    code

---
+++ 000-code.cs
@@ -0,0 +1,45 @@
+﻿pipeline{
+    agent any
+    environment {
+        DIRECTORY_PATH = "${workspace}"
+        TESTING_ENVIRONMENT = "Jackson Project"
+        PRODUCTION_ENVIRONMENT = "Jackson"
+    }
+    stages {
+        stage('Build'){
+            steps{
+                echo "fetch the source code from the directory path specified: ${env.DIRECTORY_PATH}"
+                echo "compile the code and generate any necessary artifacts"
+            }
+        }
+        stage('Test'){
+            steps{
+                echo "unit tests"
+                echo "integration tests"
+            }
+        }
+
+        stage('CodeQualityCeck'){
+            steps{
+                echo "check the quality of the code"
+            }
+        }
+        stage('Deploy'){
+            steps{
+                echo "deploy the application to a testing environment specified: ${env.TESTING_ENVIRONMENT}"
+
+            }
+        }
+        stage('Approval'){
+            steps{
+                sleep 10
+            }
+        }
+        stage('DeployToProduction'){
+            steps{
+                echo "Deploying the code to production environment: ${env.PRODUCTION_ENVIRONMENT}"
+            }
+        }
+    }
+
+}
